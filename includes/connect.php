037fa21f33211eaf6b6ebe9b9115ca8f15a04c9b
KienQuan <hotrankienquan144@gmail.com>
2022-09-27 15:35:48 +0700

    update

--- includes/connect.php
+++ includes/connect.php
@@ -1,10 +1,10 @@
 <?php 
-
+// ddang o nhanh dev_thien
 
 $conn = mysqli_connect('localhost', 'root', '', 'nhac_buh_php');
 
 if($conn) {
-  echo 'success';
+  // echo 'success';
 
 }else {
   die(mysql_error($conn));
